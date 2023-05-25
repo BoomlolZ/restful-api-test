4b53449b40f918efabab788e029a06b54315c6a7
Boommerang <boom.rang1234567890@gmail.com>
2023-05-25 11:26:40 +0700

    frist commit

--- server.js
+++ server.js
@@ -1,70 +1,119 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const Product = require('./models/product')
+// const express = require('express')
+// const app = express()
+// const mongoose = require('mongoose')
+// const User = require('./models/user')
+// const Banner = require('./models/banner')
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/node-api-101'
-const PORT = process.env.PORT || 9000
+// // const MONGODB_URI =
+// //   process.env.MONGODB_URI || 'mongodb://root:root@127.0.0.1:27017/hello?authSource=admin'
+// // const PORT = process.env.PORT || 3000
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+// // mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
-mongoose.connection.on('error', err => {
-  console.error('MongoDB error', err)
-})
+// mongoose.connection.on('error', err => {
+//   console.error('MongoDB error', err)
+// })
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/products', async (req, res) => {
-  const products = await Product.find({})
-  res.json(products)
-})
+// app.get('/user', async (req, res) => {
+//   const user = await User.find({})
+//   res.json(user)
+// })
 
-app.get('/products/:id', async (req, res) => {
-  const { id } = req.params
+// app.get('/user/:id', async (req, res) => {
+//   const { id } = req.params
 
-  try {
-    const product = await Product.findById(id)
-    res.json(product)
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
+//   try {
+//     const user = await User.findById(id)
+//     res.json(user)
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
 
-app.post('/products', async (req, res) => {
-  const payload = req.body
-  try {
-    const product = new Product(payload)
-    await product.save()
-    res.status(201).end()
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
+// app.post('/register', async (req, res) => {
+//   const payload = req.body
+  
+// })
+// app.post('/login', async (req, res) => {
+  
+// })
 
-app.put('/products/:id', async (req, res) => {
-  const payload = req.body
-  const { id } = req.params
+// app.put('/user/:id', async (req, res) => {
+//   const payload = req.body
+//   const { id } = req.params
 
-  try {
-    const product = await Product.findByIdAndUpdate(id, { $set: payload })
-    res.json(product)
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
+//   try {
+//     const user = await User.findByIdAndUpdate(id, { $set: payload })
+//     res.json({ status: 'success'})
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
 
-app.delete('/products/:id', async (req, res) => {
-  const { id } = req.params
+// app.delete('/user/:id', async (req, res) => {
+//   const { id } = req.params
 
-  try {
-    await Product.findByIdAndDelete(id)
-    res.status(204).end()
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
+//   try {
+//     await User.findByIdAndDelete(id)
+//     res.json({ status: 'success'})
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
+// //**********************************************************************Banner*******//
 
-app.listen(PORT, () => {
-  console.log(`Application is running on port ${PORT}`)
-})
+// app.get('/banner', async (req, res) => {
+//   const banner = await Banner.find({})
+//   res.json(banner)
+// })
+
+// app.get('/banner/:id', async (req, res) => {
+//   const { id } = req.params
+
+//   try {
+//     const banner = await Banner.findById(id)
+//     res.json(banner)
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
+
+// app.post('/banner', async (req, res) => {
+//   const payload = req.body
+//   try {
+//     const banner = new Banner(payload)
+//     await banner.save()
+//     res.json({ status: 'success'})
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
+
+// app.put('/banner/:id', async (req, res) => {
+//   const payload = req.body
+//   const { id } = req.params
+
+//   try {
+//     const banner = await Banner.findByIdAndUpdate(id, { $set: payload })
+//     res.json({ status: 'success'})
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
+
+// app.delete('/banner/:id', async (req, res) => {
+//   const { id } = req.params
+
+//   try {
+//     await Banner.findByIdAndDelete(id)
+//     res.json({ status: 'success'})
+//   } catch (error) {
+//     res.status(400).json(error)
+//   }
+// })
+
+// app.listen(PORT, () => {
+//   console.log(`Application is running on port ${PORT}`)
+// })
